13d8a2aee43e231ecfbf1ddeff6eb00e5ec396f3
quocanhvien99 <quocanhvien99@gmail.com>
2020-04-28 17:27:56 +0700

    Fix bug

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,18 +15,10 @@ module.exports.edit = function(req, res) {
         id: req.params.id
     });
    };
-module.exports.add = function(req, res) {
-    if (req.body.username.length > 30) {
-        res.render('users', {
-            error: 'Username không được nhập quá 30 ký tự.',
-            values: req.body,
-            users: db.get('users').value()
-        });
-    } else {
-        req.body.id = shortid.generate();
-        db.get('users').push(req.body).write();
-        res.redirect('/users');
-    }    
+module.exports.add = function(req, res) {    
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');       
 };
 module.exports.update = function(req, res) {    
     db.get('users').find({ id: req.params.id }).assign({ email: req.body.email }).write();
